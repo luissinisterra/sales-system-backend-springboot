ed4bdf63afefa9c98801295557f631d82444cb4a
Luis Eduardo Sinisterra Montaño <luiseduardosinisterramontano@gmail.com>
2025-07-02 19:47:12 -0500

    Implemented the Provider entity using validations and mapping for tables in attributes like name,m phone, email and provider products

--- src/main/java/com/api/sales_system/entity/Provider.java
+++ src/main/java/com/api/sales_system/entity/Provider.java
@@ -1,4 +1,39 @@
 package com.api.sales_system.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "providers")
 public class Provider {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "El nombre es obligatorio.")
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @NotBlank(message = "El número de celular es obligatorio.")
+    @Column(name = "phone_number", nullable = false, unique = true)
+    private String phoneNumber;
+
+    @NotBlank(message = "El correo electrónico es obligatorio.")
+    @Email(message = "Formato de correo electrónico inválido.")
+    @Column(name = "email", nullable = false, unique = true, length = 100)
+    private String email;
+
+    @OneToMany(mappedBy = "products_catalog")
+    private List<ProductCatalog> productCatalog;
+
 }
