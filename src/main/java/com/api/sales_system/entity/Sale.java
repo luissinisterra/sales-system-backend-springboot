9c7cf38d770dd186f28a17e43423d115790abae7
Luis Eduardo Sinisterra Montaño <luiseduardosinisterramontano@gmail.com>
2025-07-02 21:40:42 -0500

    Deleted the imports that i did'nt use

--- src/main/java/com/api/sales_system/entity/Sale.java
+++ src/main/java/com/api/sales_system/entity/Sale.java
@@ -2,7 +2,6 @@ package com.api.sales_system.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
